ee45f2199780967af8e36971b9999c51a4cf736b
Yumin Cho <yumin.cho@moloco.com>
2025-06-02 21:34:47 +0900

    feat: add basename to routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/id430-speculative-design/",
-  publicDir: "public",
-});
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    plugins: [react()],
+    base: env.VITE_BASE_PATH,
+    publicDir: "public",
+  });
+};
