92f638a8452f23aed90a49d38b8bb44febfd36ce
Yumin Cho <yumin.cho@moloco.com>
2025-06-07 00:05:32 +0900

    Add common `Store` page

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,17 +1,18 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useTheme, type DefaultTheme } from "styled-components";
 
 // White mode pages
 import Home from "../pages/white/Home";
-import Store from "../pages/white/Store";
 import History from "../pages/white/History";
 
 // Dark mode pages
 import HomeDark from "../pages/dark/Home";
-import StoreDark from "../pages/dark/Store";
 import HistoryDark from "../pages/dark/History";
+
+// Common pages
 import Article from "../pages/common/Article";
-import { useTheme, type DefaultTheme } from "styled-components";
 import Stories from "../pages/common/Stories";
+import Store from "../pages/common/Store";
 
 export default function AppRouter() {
   const { mode }: DefaultTheme = useTheme();
@@ -26,10 +27,7 @@ export default function AppRouter() {
         path="/home"
         element={mode === "white" ? <Home /> : <HomeDark />}
       />
-      <Route
-        path="/store"
-        element={mode === "white" ? <Store /> : <StoreDark />}
-      />
+      <Route path="/store" element={<Store />} />
       <Route
         path="/history"
         element={mode === "white" ? <History /> : <HistoryDark />}
--- src/pages/common/Store.tsx
+++ src/pages/common/Store.tsx
@@ -1,8 +1,10 @@
-export default function Store() {
+function Store() {
   return (
     <div className="page">
       <h1>Store Page</h1>
       <p>This is the store page of your application.</p>
     </div>
   );
-} 
+}
+
+export default Store;
